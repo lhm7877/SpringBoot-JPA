199aebec0e8c887e69328b39ffd95b1255b00bf6
lhm7877 <vkfdldh7>
2021-11-23 01:50:40 +0900

    test: member create,find 테스트 코드 작성

---
+++ hoomin/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import static org.junit.Assert.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Test
+	@Transactional
+	public void testMember() {
+		// given
+		Member member = new Member();
+		member.setUsername("memberA");
+
+		// when
+		final Long saveId = memberRepository.save(member);
+		final Member findMember = memberRepository.find(saveId);
+
+		//then
+		Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+		Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+	}
+}
