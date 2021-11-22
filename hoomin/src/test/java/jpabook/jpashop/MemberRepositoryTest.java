99401d3d55e8d603182031ebdf21dcc82efa9c6b
lhm7877 <vkfdldh7>
2021-11-23 01:56:11 +0900

    build: p6spy 라이브러리 추가

--- hoomin/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ hoomin/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ public class MemberRepositoryTest {
 
 	@Test
 	@Transactional
+	@Rollback(value = false)
 	public void testMember() {
 		// given
 		Member member = new Member();
@@ -30,5 +32,6 @@ public class MemberRepositoryTest {
 		//then
 		Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
 		Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+		Assertions.assertThat(findMember).isEqualTo(member);
 	}
 }
