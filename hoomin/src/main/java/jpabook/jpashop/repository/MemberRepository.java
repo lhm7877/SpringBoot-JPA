b97d2ea4199e3007104bf069d6f2a5cec858fb5f
lhm7877 <lhm7877@gmail.com>
2021-11-30 00:47:04 +0900

    feat: add MemberService

--- hoomin/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ hoomin/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -12,9 +12,11 @@ import jpabook.jpashop.domain.Member;
 @Repository
 public class MemberRepository {
 
+	// Springboot가 이 어노테이션이 있으면 엔티티 매니저 설정해준다.
 	@PersistenceContext
 	private EntityManager em;
 
+	// 커맨드와 쿼리를 분리해라!
 	public void save(Member member) {
 		em.persist(member);
 	}
