128e478e7612d92ff17ebbd5f5c7628a8b1b24b7
Hyungwoo Kim <52458039+Wu22e@users.noreply.github.com>
2021-12-05 18:02:27 +0900

    docs: section 7 실습 코드

--- hyungwoo/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ hyungwoo/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,14 @@ public class ItemService {
 	@Transactional
 	public void saveItem(Item item) {
 		itemRepository.save(item);
+	}
+
+	@Transactional
+	public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+		Item findItem = itemRepository.findOne(itemId);
+		findItem.setPrice(price);
+		findItem.setName(name);
+		findItem.setStockQuantity(stockQuantity);
 	}
 
 	public List<Item> findItems() {
